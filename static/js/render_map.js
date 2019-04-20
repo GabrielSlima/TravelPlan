65c1fa4700b9fd491863205b0b855b4841db055b
Gabriel <gabriel.lima@egs.com.br>
2019-04-19 22:05:26 -0300

    Add the mouseEvent to our app

--- static/js/render_map.js
+++ static/js/render_map.js
@@ -180,41 +180,117 @@ var view = {
 }
 
 // THIS IS THE MODEL FOR OU PLACES
-function MyFavoritePlace( id, name, location) {
+
+function ArticlesAboutMyFavPlaces(favPlaceId, articles)
+{
+    self = this;
+    self.favPlaceId = favPlaceId;
+    self.articles = articles;
+}
+function MyFavoritePlace( id, name, location, articles) {
     self = this;
     self.id = id;
     self.name = name;
     self.location = location;
+    self.articles = articles;
 }
 
 // THIS IS THE VIEWMODEL FOR HTML
 function ListViewModel(){
     var self = this;
+
+    var articles  = [];
+
     self.filterValue = ko.observable('');
     self.places = ko.observableArray([
-            new MyFavoritePlace(1, "Dubai, United Arab Emirates", {lat: 25.238317, lng: 55.272560}),
-            new MyFavoritePlace(2, "Dublin, Ireland", {lat:  53.350418, lng: -6.256612 }),
-            new MyFavoritePlace(3, "Madrid, Spain", {lat:  40.432754, lng: -3.701094 }),
-            new MyFavoritePlace(4, "Barcelona, Spain", {lat: 41.386752, lng: 2.170314 }),
-            new MyFavoritePlace(5, "CN Tower, Canada", {lat:  43.642729, lng: -79.387743 }),
-            new MyFavoritePlace(6, "Moraine Lake, Canada", {lat:  51.322650, lng: -116.186015 }),
-            new MyFavoritePlace(7, "Gramado, Brazil", {lat:  -29.373412, lng: -50.876430 }),
-            new MyFavoritePlace(8, "Burbank, USA", {lat: 49.342805, lng: -123.114882 }),
-            new MyFavoritePlace(9, "Horseshoe Falls, Canada", {lat:  43.079570, lng: -79.075073 }),
-            new MyFavoritePlace(10, "Lake Louise, Canada", {lat:  51.429395, lng: -116.175532 }),
-            new MyFavoritePlace(11, "Skylon Tower, Canada", {lat: 43.085422, lng: -79.079026 }),
-            new MyFavoritePlace(12, "Sunshine Village, Canada", {lat: 51.115341, lng: -115.761881 }),
-            new MyFavoritePlace(13, "Suomenlinna - Helsinki, Finland", {lat: 60.148162, lng: 24.987430  }),
-            new MyFavoritePlace(14, "Milford Sound, New Zeland", {lat:-44.636473, lng: 167.898747})
+            new MyFavoritePlace(1, "Dubai, United Arab Emirates", {lat: 25.238317, lng: 55.272560}, []),
+            new MyFavoritePlace(2, "Dublin, Ireland", {lat:  53.350418, lng: -6.256612 }, []),
+            new MyFavoritePlace(3, "Madrid, Spain", {lat:  40.432754, lng: -3.701094 }, []),
+            new MyFavoritePlace(4, "Barcelona, Spain", {lat: 41.386752, lng: 2.170314 }, []),
+            new MyFavoritePlace(5, "CN Tower, Canada", {lat:  43.642729, lng: -79.387743 }, []),
+            new MyFavoritePlace(6, "Moraine Lake, Canada", {lat:  51.322650, lng: -116.186015 }, []),
+            new MyFavoritePlace(7, "Gramado, Brazil", {lat:  -29.373412, lng: -50.876430 }, []),
+            new MyFavoritePlace(8, "Burbank, USA", {lat: 49.342805, lng: -123.114882 }, []),
+            new MyFavoritePlace(9, "Horseshoe Falls, Canada", {lat:  43.079570, lng: -79.075073 }, []),
+            new MyFavoritePlace(10, "Lake Louise, Canada", {lat:  51.429395, lng: -116.175532 }, []),
+            new MyFavoritePlace(11, "Skylon Tower, Canada", {lat: 43.085422, lng: -79.079026 }, []),
+            new MyFavoritePlace(12, "Sunshine Village, Canada", {lat: 51.115341, lng: -115.761881 }, []),
+            new MyFavoritePlace(13, "Suomenlinna - Helsinki, Finland", {lat: 60.148162, lng: 24.987430  }, []),
+            new MyFavoritePlace(14, "Milford Sound, New Zeland", {lat:-44.636473, lng: 167.898747}, [])
         ]);
-    
+
+        console.log(self.places());
+        
+    self.getArticles = function(id,location){
+        console.log(id);
+        $.ajax({
+            type: 'GET',
+            processData: false,
+            url: 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + location + '&api-key=pdcnycvY5voepeY1l4ec0nVQVZWcUeNz',
+            success: function(data){
+                console.log('Success');
+                var article = new ArticlesAboutMyFavPlaces(id, data.response.docs);
+                articles.push(article);
+                console.log('Actual items inside article list:')
+                console.log(articles);
+
+                console.log('Trying to get the item with id' + id);
+                console.log(articles.filter(item => item.favPlaceId === id));
+                $('#'+id).parent('li').addClass('mouseEvent');
+                $('.mouseEvent').mouseover(function(){
+                    console.log('Abrir o overlay com uma lista com os artigos do objeto recuperado.');
+                    var current_id = $(this).children('a').attr('id');
+                    var current_item;
+                    console.log('This is the id of the actual item: ' + current_id);
+                    for(var i = 0; i < articles.length; i++) {
+                        console.log(current_id + ' is equal to ' + articles[i].favPlaceId);
+                        if(articles[i].favPlaceId == current_id) current_item = articles[i] 
+
+                    }
+                    console.log(current_item);
+                });
+            
+                $('.mouseEvent').mouseout(function(){
+                    console.log('Deveria TIRAR a div com os dados');
+                });
+            },
+            error: function() {
+                console.log('Error');
+                var article = new ArticlesAboutMyFavPlaces(id, 'Error');
+                articles.push(article);
+                $('#'+id).parent('li').addClass('mouseEvent');
+                $('.mouseEvent').mouseover(function(){
+                    var current_id = $(this).children('a').attr('id');
+                    var current_item;
+                    console.log('This is the id of the actual item: ' + current_id);
+                    for(var i = 0; i < articles.length; i++) {
+                        console.log(current_id + ' is equal to ' + articles[i].favPlaceId);
+                        if(articles[i].favPlaceId == current_id) current_item = articles[i] 
+
+                    }
+                    console.log(current_item);
+
+                });
+            
+                $('.mouseEvent').mouseout(function(){
+                    console.log(this.innerHTML);
+                });
+            }
+        });
+    }
+
+    for(var i = 0; i<self.places().length; i++) {
+        console.log(self.places()[i].articles);
+        self.getArticles(self.places()[i].id, self.places()[i].name);
+    }
+
     self.openInfoWindow = function(element){
 
         // CLOSE ALL OPENED INFOWINDOWS
         for (var j = 0; j < mapModel.markers.length; j++ ) new google.maps.event.trigger(mapModel.markers[j], 'closeClick' );
 
         var item = mapModel.collectionOfMarkersWithId.filter(item => item.id === '#' + element.id);
-        
+   
         new google.maps.event.trigger(item[0].marker, 'click' );
     }
 
@@ -223,6 +299,7 @@ function ListViewModel(){
     }
     // THIS FILTER THE LIST BASED ON THE INPUT DATA
     self.filteredList = ko.computed(function (){
+
         
         self.clearMap();
         
